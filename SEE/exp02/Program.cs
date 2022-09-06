9dc7ae357e05ba6d233d8645c26302ae7b4c31ab
Din <hurshid.17.97@mail.ru>
2022-09-06 23:56:24 +0500

    exp02

---
+++ SEE/exp02/Program.cs
@@ -0,0 +1,16 @@
+﻿// задача 15
+
+Console.Write("Введите цифру день недели: ");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+void CheckingTheDayOfTheWeek (int dayNumber) {
+  if (dayNumber == 6 || dayNumber == 7) {
+  Console.WriteLine("этот день выходной");
+  }
+  else if (dayNumber < 1 || dayNumber > 7) {
+    Console.WriteLine("нет такого дня недели!");
+  }
+  else Console.WriteLine("не выходной день ");
+}
+
+CheckingTheDayOfTheWeek(dayNumber);
