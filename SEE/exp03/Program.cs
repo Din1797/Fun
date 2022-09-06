779444c413a4a44aeb3de59b67509fc02ddf6ba1
Din <hurshid.17.97@mail.ru>
2022-09-07 00:55:49 +0500

    exp03

---
+++ SEE/exp03/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 13
+
+Console.Write("Введи число: ");
+int Number = Convert.ToInt32(Console.ReadLine());
+
+if (Number > 99)
+    {
+        int count = Number;
+        while (count > 999)
+            {
+                count /= 10;
+            }
+        Console.WriteLine($"третья цифра " + count % 10);
+    }
+else 
+Console.Write("нет третьей цифры ");
